3d0fda92ee76660b80e96a53c59462d79fb054fe
Yeji <yeji@kimbarros.com>
2015-10-15 19:29:09 -0400

    * repositories.config: * StackHistory.cs: * OOPsIDidItAgain.csproj:
    * OOPsIDidItAgain.userprefs: StackHistory added

---
+++ OOPsIDidItAgain/StackHistory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOPsIDidItAgain
+{
+	public class StackHistory<T>
+	{
+		private Node first; // top of the stack
+
+		public bool isEmpty { get { return first == null; } }
+
+		public void push(string element) {
+			Node old = first;
+			first = new Node();
+			first.userInput = element;
+			first.next = old;
+		}
+
+		public string pop() {
+			if (isEmpty)
+				throw new Exception ("Stack Underflow: No elements in the stack");
+			string element = first.userInput;
+			first = first.next; // the element after the top gets to be top now
+			return element.ToString();
+		}
+
+		public string peek() {
+			if (isEmpty)
+				throw new Exception ("Stack Underflow: No elements in the stack");
+			return first.userInput.ToString ();
+		}
+			
+	}
+	public class Node {
+		public string userInput { get; set; }
+		public Node next { get; set; }
+	}
+}
+
